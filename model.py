f451567d2ca5f509f27bc8ebc299297cf11f5c0e
Adelaide Chen <adelaide01>
2013-04-26 15:29:50 -0700

    first commit

--- model.py
+++ model.py
@@ -1,16 +1,57 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+    
+    """Import columns from U.user"""
+    id = Column(Integer, primary_key = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
 
+
+class Movie(Base):
+    """Import columns from U.item"""
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key = True)
+    title = Column(String(64), nullable = False)
+    released_at = Column(String(64), nullable = True)
+    url = Column(String(300), nullable = True)
+
+
+class Rating(Base):
+    """Import columns from U.data"""
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = True)
+    movie_id = Column(Integer, nullable = True)
+    rating = Column(Integer, nullable = False)
+    
 
 ### End class declarations
 
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind = ENGINE)
+
+    return Session()
+
 def main():
-    """In case we need this for something"""
+    """Create main function just in case."""
     pass
 
 if __name__ == "__main__":
--- seed.py
+++ seed.py
@@ -1,22 +1,74 @@
-import model
+"""
+seed.py
+
+"""
+
 import csv
+import model
+import datetime
+
+""" CSV files are hardcoded, but another option is args. """
+#import sys
+#script, filename = argv
+# filepath = sys.argv
+# print filepath
+# print "Parsing: %s" % filepath
 
 def load_users(session):
-    # use u.user
-    pass
+   """ Import file u.user into database. """
+    
+    with open("seed/u.user") as f:
+        reader = csv.reader(f, delimiter = "|")
+        for row in reader:
+            user = model.User(id=row[0], age=row[1], zipcode=row[4])
+            session.add(user)
 
 def load_movies(session):
-    # use u.item
-    pass
+    """ Import file u.item into database. """
+    """ Decode movie title from Latin-1 format to database friendly unicode. """
+    """ Make date string into a datetime format of day, month, year. """
+
+    with open("seed/u.item") as f:
+        reader = csv.reader(f, delimiter = "|")
+        
+        for row in reader:
+            # Set id = to row 0.
+            id = int(row[0])
+            # Set movie title = to row 1 and decode from UTF-8 to Latin-1.
+            title = row[1].decode("latin-1")
+            
+            # Format date in %d-%b-%Y or day-month-year or 00-00-0000.
+            released_at = row[2]      
+            if released_at: # Applies to places where a date exists. Sometimes dates don't exist.
+                formatted_date = datetime.datetime.strptime(released_at, "%d-%b-%Y")
+            else:
+                None # No format for empty date fields.
+            
+            # Set URL = to row 4.            
+            url = row[4]
+
+            movie = model.Movie(id=id, title=title, released_at=formatted_date, url=url)    
+            session.add(movie)
+
 
 def load_ratings(session):
-    # use u.data
-    pass
+    """ Import file u.data into database."""
+
+    with open("seed/u.data") as f:
+        reader = csv.reader(f, delimiter = "\t")
+
+        for row in reader:
+            rating = model.Rating(user_id=row[0], movie_id=row[1], rating=row[2])
+            session.add(rating)
+
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    """ Call each of the load_* functions with the session as an argument. """
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
+    session.commit()
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
